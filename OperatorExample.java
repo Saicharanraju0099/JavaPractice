531383a41677bf05975364223e58b34191803d43
saicharan <saicharan@saicharans-MacBook-Air.local>
2024-06-26 10:46:57 +0530

    fir Commit

---
+++ OperatorExample.java
@@ -0,0 +1,27 @@
+/* 
+public class OperatorExample{  
+public static void main(String args[]){  
+int a=10;  
+int b=10;  
+int c = 40;
+int d = 20;
+System.out.println(c/d);
+System.out.println(--a + ++a);//10+12=22  
+System.out.println(--b + b++);//10+11=21  
+  
+}}  */
+
+        
+
+        
+}
+}
+
+    
+        
+
+
+
+    
+
+
---
+++ test.java
@@ -0,0 +1,9 @@
+package java.com;
+
+public class test{
+
+public static void main(str [] args){
+	system.out.println("Jfojo");
+	}
+}
+
